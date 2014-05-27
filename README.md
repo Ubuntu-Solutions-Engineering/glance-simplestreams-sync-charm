c14b18ff857d0436e80ffbb118443113a3d19f90
Michael McCracken <mike.mccracken@canonical.com>
2014-05-27 17:33:22 +0000

    Add AGPL license info

--- README.md
+++ README.md
@@ -14,3 +14,17 @@ TODO
 
 TODO
 
+# Copyright
+
+The glance-simplestreams sync charm is free software: you can
+redistribute it and/or modify it under the terms of the GNU Affero General
+Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+The charm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this charm.  If not, see <http://www.gnu.org/licenses/>.
--- scripts/glance-simplestreams-sync.py
+++ scripts/glance-simplestreams-sync.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python2.7
 #
 # Copyright 2014 Canonical Ltd.
+#
+# This file is part of the glance-simplestreams sync charm.
+
+# The glance-simplestreams sync charm is free software: you can
+# redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# The charm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this charm.  If not, see <http://www.gnu.org/licenses/>.
 
 # This script runs as a cron job installed by the
 # glance-simplestreams-sync juju charm.  It reads config files that
